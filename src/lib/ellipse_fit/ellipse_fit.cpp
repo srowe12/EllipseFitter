ced77b2d585d7f159f8352bca103f1cb8c9b5c86
Stephen Rowe <srowe12@gmail.com>
2016-05-01 16:42:58 -0400

    Accelerate some functions with fixed sizes using benchmarks
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/ellipse_fit/ellipse_fit.cpp
+++ src/lib/ellipse_fit/ellipse_fit.cpp
@@ -23,31 +23,6 @@ void AssembleMatrices(const arma::vec& x, const arma::vec& y, arma::mat& M, arma
     D.col(5) = arma::ones(nrows,1);
     arma::mat S = D.t() * D;
 
-    auto S1 = S.submat(0,0,2,2);
-    auto S2 = S.submat(0,3,2,5);
-    auto S3 = S.submat(3,3,5,5);   
-    T = -inv(S3)*S2.t();
-    M = S1 + S2*T;
-    
-    ///@todo may be faster to multiply through by .5 then by -2 on row(1)
-    M.row(2) *= .5;
-    M.row(1) *=  -1;
-    M.row(0) *= .5;
-    M.swap_rows(0,2);
-
-}
-
-void AssembleMatricesFast(const arma::vec& x, const arma::vec& y, arma::mat& M, arma::mat& T) {
-    size_t nrows = x.size();
-    arma::mat D(nrows,6);
-    D.col(0) = x % x;
-    D.col(1) = x % y;
-    D.col(2) = y % y;
-    D.col(3) = x;
-    D.col(4) = y;
-    D.col(5) = arma::ones(nrows,1);
-    arma::mat S = D.t() * D;
-
     const auto& S1 = S.submat(0,0,2,2);
     const auto& S2 = S.submat(0,3,2,5);
     const auto& S3 = S.submat(3,3,5,5);   
@@ -59,6 +34,18 @@ void AssembleMatricesFast(const arma::vec& x, const arma::vec& y, arma::mat& M, 
     M.row(1) *=  -1;
     M.row(0) *= .5;
     M.swap_rows(0,2);
+
+}
+
+arma::vec ComputeOptimalEigenvector(const arma::mat::fixed<3,3>& M) {
+   arma::cx_vec eigval(3);
+   arma::cx_mat eigvec_complex(3,3);
+ 
+   eig_gen(eigval, eigvec_complex, M);
+   ///@todo Hmmm, this seems gross having to cast as a real vector.
+   arma::mat eigvec = real(eigvec_complex);
+   arma::rowvec cond = 4*eigvec.row(0) % eigvec.row(2) - eigvec.row(1) % eigvec.row(1);
+   return eigvec.cols(arma::find(cond > 0));
 
 }
 
--- src/lib/ellipse_fit/tests/ellipse_fit_speed_test.cpp
+++ src/lib/ellipse_fit/tests/ellipse_fit_speed_test.cpp
@@ -3,6 +3,47 @@
 #include <iostream>
 #include <chrono>
 
+void AssembleMatricesFast(const arma::vec& x, const arma::vec& y, arma::mat::fixed<3,3>& M, arma::mat::fixed<3,3>& T);
+
+arma::vec ComputeOptimalEigenvectorDynamic(const arma::mat::fixed<3,3>& M);
+
+void AssembleMatricesFast(const arma::vec& x, const arma::vec& y, arma::mat::fixed<3,3>& M, arma::mat::fixed<3,3>& T){
+    size_t nrows = x.size();
+    arma::mat D(nrows,6);
+    D.col(0) = x % x;
+    D.col(1) = x % y;
+    D.col(2) = y % y;
+    D.col(3) = x;
+    D.col(4) = y;
+    D.col(5) = arma::ones(nrows,1);
+    arma::mat S = D.t() * D;
+
+    const auto& S1 = S.submat(0,0,2,2);
+    const auto& S2 = S.submat(0,3,2,5);
+    const auto& S3 = S.submat(3,3,5,5);   
+    T = -inv(S3)*S2.t();
+    M = S1 + S2*T;
+    
+    ///@todo may be faster to multiply through by .5 then by -2 on row(1)
+    M.row(2) *= .5;
+    M.row(1) *=  -1;
+    M.row(0) *= .5;
+    M.swap_rows(0,2);
+
+}
+
+arma::vec ComputeOptimalEigenvectorDynamic(const arma::mat& M) {
+   arma::cx_vec eigval;
+   arma::cx_mat eigvec_complex;
+ 
+   eig_gen(eigval, eigvec_complex, M);
+   ///@todo Hmmm, this seems gross having to cast as a real vector.
+   arma::mat eigvec = real(eigvec_complex);
+   arma::rowvec cond = 4*eigvec.row(0) % eigvec.row(2) - eigvec.row(1) % eigvec.row(1);
+   return eigvec.cols(arma::find(cond > 0));
+
+}
+
 int main() {
    //Build some data
 
@@ -74,12 +115,27 @@ int main() {
    diff = end - start;
    std::cout << "Current Matrix Assembly:" << std::chrono::duration<double, std::milli> (diff).count() << " ms" << std::endl;
   
-   arma::mat M2(3,3); 
-   arma::mat T2(3,3);
+   arma::mat::fixed<3,3> M2; 
+   arma::mat::fixed<3,3> T2;
 
    start = std::chrono::steady_clock::now();
    AssembleMatricesFast(xin,yin,M2,T2); 
    end = std::chrono::steady_clock::now();
    diff = end - start;
    std::cout << "Alternative Matrix Assembly" <<  std::chrono::duration<double, std::milli> (diff).count() << " ms" << std::endl;
+
+
+   arma::mat Meig{ {-1.865454545454545454, 0, 1.04931818181818181}, { 0, -3.42, 0}, {3.3163636363636363, 0, -1.86545454545454}};
+   start = std::chrono::steady_clock::now();
+   arma::vec optimal_eigenvector = ComputeOptimalEigenvectorDynamic(Meig);
+   end = std::chrono::steady_clock::now();
+   diff = end - start;
+   std::cout << "Compute Eigenvalue with Dynamic matrix" <<  std::chrono::duration<double, std::milli> (diff).count() << " ms" << std::endl;
+
+   arma::mat::fixed<3,3> Meigfixed{ {-1.865454545454545454, 0, 1.04931818181818181}, { 0, -3.42, 0}, {3.3163636363636363, 0, -1.86545454545454}};
+   start = std::chrono::steady_clock::now();
+   arma::vec optimal_eigenvector2 = ComputeOptimalEigenvector(Meigfixed);
+   end = std::chrono::steady_clock::now();
+   diff = end - start;
+   std::cout << "Compute Eigenvalue with Fixed Matrix" <<  std::chrono::duration<double, std::milli> (diff).count() << " ms" << std::endl;
 }
