ed9a8f6f83272eba5d56e091596d775f3377bb44
Stephen Rowe <srowe12@gmail.com>
2016-05-01 14:01:37 -0400

    Add tests for rotated and rotated+shifted ellipses
    Signed-off-by: Stephen Rowe <srowe12@gmail.com>

--- src/lib/ellipse_fit/tests/ellipse_fit_tests.cpp
+++ src/lib/ellipse_fit/tests/ellipse_fit_tests.cpp
@@ -81,6 +81,105 @@ TEST(EllipseFitTests, FitEllipse) {
     }
 }
 
+TEST(EllipseFitTests, FitEllipseRotated) {
+
+   // Draw an ellipse
+   size_t num_samples = 20;
+   std::vector<double> x(num_samples);
+   std::vector<double> y(num_samples);
+   
+   double angle_delta = 2*M_PI/(num_samples-1);
+
+   
+   double a = 4; //Semi major axis
+   double b = 3; //Semi minor axis
+   // Sample the ellipse with points (x,y) = (a*cos(t), b*sin(t))
+   double t = 0;
+   for (size_t i = 0; i<num_samples; ++i) {
+       x[i] = a*cos(t);
+       y[i] = b*sin(t);
+       t += angle_delta;
+   }
+   // Rotate points by 45 degrees;
+   double angle_radians = 45*M_PI/180.0; //degrees
+   double cos_angle = cos(angle_radians);
+   double sin_angle = sin(angle_radians);
+   arma::mat R{{cos_angle, -sin_angle},{sin_angle,cos_angle}};
+   arma::rowvec xvec(x);
+   arma::rowvec yvec(y);
+
+   arma::mat data(2,num_samples);
+   data.row(0) = xvec;
+   data.row(1) = yvec;
+   
+   arma::mat data_rotated = R*data;
+   xvec = data_rotated.row(0);
+   yvec = data_rotated.row(1);
+   std::vector<double> x_in = arma::conv_to<std::vector<double>>::from(xvec);
+   std::vector<double> y_in = arma::conv_to<std::vector<double>>::from(yvec);
+   
+
+   std::vector<double> ellipse_coefs = FitEllipse(x_in,y_in);
+   std::vector<double> ellipse_coefs_expected{-.65743960379771, .368166178126752, -.657439603797772,0,0,7.573704235750322};
+   for(size_t i =0; i<ellipse_coefs_expected.size(); ++i) {
+       // They may differ by a sign, so normalize by the constant coefficient to handle sign
+       EXPECT_NEAR(ellipse_coefs_expected[i]/ellipse_coefs_expected[5], ellipse_coefs[i]/ellipse_coefs[5],1e-12);
+    }
+}
+
+TEST(EllipseFitTests, FitEllipseRotatedAndShifted) {
+
+   // Draw an ellipse
+   size_t num_samples = 20;
+   std::vector<double> x(num_samples);
+   std::vector<double> y(num_samples);
+   
+   double angle_delta = 2*M_PI/(num_samples-1);
+
+   
+   double a = 4; //Semi major axis
+   double b = 3; //Semi minor axis
+   // Sample the ellipse with points (x,y) = (a*cos(t), b*sin(t))
+   double t = 0;
+   for (size_t i = 0; i<num_samples; ++i) {
+       x[i] = a*cos(t);
+       y[i] = b*sin(t);
+       t += angle_delta;
+   }
+   // Rotate points by 45 degrees;
+   double angle_radians = 45*M_PI/180.0; //degrees
+   double cos_angle = cos(angle_radians);
+   double sin_angle = sin(angle_radians);
+   arma::mat R{{cos_angle, -sin_angle},{sin_angle,cos_angle}};
+   arma::rowvec xvec(x);
+   arma::rowvec yvec(y);
+
+   arma::mat data(2,num_samples);
+   data.row(0) = xvec;
+   data.row(1) = yvec;
+   
+   arma::mat data_rotated = R*data;
+   xvec = data_rotated.row(0);
+   yvec = data_rotated.row(1);
+   
+   std::vector<double> x_in = arma::conv_to<std::vector<double>>::from(xvec);
+   std::vector<double> y_in = arma::conv_to<std::vector<double>>::from(yvec);
+   //Now shift the x values by 1 and the y values by 2 to give it a center of (1,2);
+   double x_center = 1;
+   double y_center = 2;
+   for (size_t i =0; i<x_in.size(); ++i) {
+       x_in[i] += x_center;
+       y_in[i] += y_center;
+   }
+
+   std::vector<double> ellipse_coefs = FitEllipse(x_in,y_in);
+   std::vector<double> ellipse_coefs_expected{-.65743960379771, .368166178126752, -.657439603797772,.578546851342042,2.261592237064336,5.022838573014967};
+   for(size_t i =0; i<ellipse_coefs_expected.size(); ++i) {
+       // They may differ by a sign, so normalize by the constant coefficient to handle sign
+       EXPECT_NEAR(ellipse_coefs_expected[i]/ellipse_coefs_expected[5], ellipse_coefs[i]/ellipse_coefs[5],1e-12);
+    }
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
 
